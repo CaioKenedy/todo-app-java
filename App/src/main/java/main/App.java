3043626d448c0c4c870815c73ca18dc07446574f
CaioKenedy <CaioKenedySantos@gmail.com>
2022-06-11 11:05:03 -0300

    added packages

--- App/src/main/java/main/App.java
+++ App/src/main/java/main/App.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+
 
 package main;
 
 /**
- *
+ 
  * @author caiok
  */
 public class App {
