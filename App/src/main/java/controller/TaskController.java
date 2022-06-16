d0f36f61d08a22e793e1c2f309f7fe2c096de54c
CaioKenedy <CaioKenedySantos@gmail.com>
2022-06-16 12:43:06 -0300

    fixing errors and started ProjectController

--- App/src/main/java/controller/TaskController.java
+++ App/src/main/java/controller/TaskController.java
@@ -28,7 +28,7 @@ public class TaskController {
 
         Connection connection = null;
         PreparedStatement statement = null;
-        
+
         try {
             connection = ConnectionFactory.getConnection();
             statement = connection.prepareStatement(sql);
@@ -41,33 +41,33 @@ public class TaskController {
             statement.setDate(7, new Date(task.getCreatedAt().getTime()));
             statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
             statement.execute();
-            
+
         } catch (Exception ex) {
             throw new RuntimeException("Erro ao salvar tarefa "
                     + ex.getMessage(), ex);
-            
+
         } finally {
             ConnectionFactory.closeConnection(connection, statement);
         }
-        
+
     }
 
     public void update(Task task) {
-        
+
         String sql = "UPDATE task SET "
                 + "idProject = ?, "
                 + "name = ?, "
                 + "description = ?, "
                 + "notes = ?, "
                 + "completed = ?, "
-                + "deadline = ?, " 
+                + "deadline = ?, "
                 + "createdAt = ?, "
                 + "updatedAt = ?, "
                 + "WHERE ID = ?";
-        
+
         Connection connection = null;
         PreparedStatement statement = null;
-        
+
         try {
             connection = ConnectionFactory.getConnection();
             statement = connection.prepareStatement(sql);
@@ -83,7 +83,6 @@ public class TaskController {
             throw new RuntimeException("Erro ao atualizar a tarefa"
                     + ex.getMessage(), ex);
         }
-        
 
     }
 
@@ -107,35 +106,43 @@ public class TaskController {
     }
 
     public List<Task> getAll(int idProject) {
-        
+
         String sql = "SELECT * FROM tasks WHERE idProject = ?";
-        
+
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
-        
+
         List<Task> tasks = new ArrayList<Task>();
-        
-        
+
         try {
             connection = ConnectionFactory.getConnection();
             statement = connection.prepareStatement(sql);
             statement.setInt(1, idProject);
             resultSet = statement.executeQuery();
-            
+
             while (resultSet.next()) {
                 Task task = new Task();
                 task.setId(resultSet.getInt("id"));
                 task.setIdProject(resultSet.getInt("idProject"));
                 task.setName(resultSet.getString("name"));
                 task.setDescription(resultSet.getString("description"));
-            
+                task.setNotes(resultSet.getString("notes"));
+                task.setCompleted(resultSet.getBoolean("completed"));
+                task.setDeadline(resultSet.getDate("deadline"));
+                task.setCreatedAt(resultSet.getDate("createdAt"));
+                task.setUpdatedAt(resultSet.getDate("updatedAt"));
+
+                tasks.add(task);
+
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao atualizar a tarefa"
+                    + ex.getMessage(), ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
         }
-        } catch (Exception e) {
-        }
-        
-        
-        
+
         return null;
     }
 }
